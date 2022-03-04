53e60e25ca8d8bd247b80b83c0af93dcaa38d89d
SupurCalvinHiggins <calvin@supurloop.com>
2022-03-03 20:00:42 -0500

    Added benchmark and installer script.

---
+++ benchmark.py
@@ -0,0 +1,23 @@
+import os
+import glob
+import subprocess
+
+try:
+	from termcolor import colored
+except:
+	print("*** WARNING ***")
+	print("Termcolor module not installed")
+	colored = lambda x : x
+
+if __name__ == "__main__":
+	for fname in glob.glob("**/*.bench.cpp"):
+		print(colored("*** CURRENT FILE ***", "green"))
+		print(fname)
+		print(colored("*** COMPILING ***", "green"))
+		subprocess.run(["g++", fname, "-std=c++17", "-lbenchmark", "-lpthread", "-o", "benchmark"])
+		if os.path.exists("benchmark"):
+			print(colored("*** RUNNING ***", "green"))
+			subprocess.run(["./benchmark"])
+		else:
+			print(colored("*** WARNING ***", "yellow"))
+			print(f"{fname} failed to compile")
