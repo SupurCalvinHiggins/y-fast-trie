10e40db739d70196d19fb41bb3539d5d3e571fc3
SupurCalvinHiggins <calvin@supurloop.com>
2022-03-04 12:26:43 -0500

    Updated tests to use templating.

--- x-fast-trie/x-fast-trie.benchmark.cpp
+++ x-fast-trie/x-fast-trie.benchmark.cpp
@@ -15,7 +15,7 @@ static void BM_XFastTrie_n_insert(benchmark::State& state) {
 		}
 	}
 }
-BENCHMARK(BM_XFastTrie_n_insert)->RangeMultiplier(2)->Range(1, 1<<16)->Setup(seed);
+BENCHMARK(BM_XFastTrie_n_insert)->RangeMultiplier(16)->Range(1, 1<<16)->Setup(seed);
 
 static void BM_XFastTrie_1_predecessor_on_n_trie(benchmark::State& state) {
 	XFastTrie<uint64_t> trie;
@@ -29,7 +29,7 @@ static void BM_XFastTrie_1_predecessor_on_n_trie(benchmark::State& state) {
 		benchmark::DoNotOptimize(trie.predecessor(val));
 	}
 }
-BENCHMARK(BM_XFastTrie_1_predecessor_on_n_trie)->RangeMultiplier(2)->Range(1, 1<<16)->Setup(seed);
+BENCHMARK(BM_XFastTrie_1_predecessor_on_n_trie)->RangeMultiplier(16)->Range(1, 1<<16)->Setup(seed);
 
 static void BM_XFastTrie_1_successor_on_n_trie(benchmark::State& state) {
 	XFastTrie<uint64_t> trie;
@@ -43,7 +43,7 @@ static void BM_XFastTrie_1_successor_on_n_trie(benchmark::State& state) {
 		benchmark::DoNotOptimize(trie.successor(val));
 	}
 }
-BENCHMARK(BM_XFastTrie_1_successor_on_n_trie)->RangeMultiplier(2)->Range(1, 1<<16)->Setup(seed);
+BENCHMARK(BM_XFastTrie_1_successor_on_n_trie)->RangeMultiplier(16)->Range(1, 1<<16)->Setup(seed);
 
 static void BM_XFastTrie_1_contains_on_n_trie(benchmark::State& state) {
 	XFastTrie<uint64_t> trie;
@@ -57,6 +57,6 @@ static void BM_XFastTrie_1_contains_on_n_trie(benchmark::State& state) {
 		benchmark::DoNotOptimize(trie.contains(val));
 	}
 }
-BENCHMARK(BM_XFastTrie_1_contains_on_n_trie)->RangeMultiplier(2)->Range(1, 1<<16)->Setup(seed);
+BENCHMARK(BM_XFastTrie_1_contains_on_n_trie)->RangeMultiplier(16)->Range(1, 1<<16)->Setup(seed);
 
 BENCHMARK_MAIN();
--- x-fast-trie/x-fast-trie.test.cpp
+++ x-fast-trie/x-fast-trie.test.cpp
@@ -1,319 +1,139 @@
 #include "x-fast-trie.h"
 #include <gtest/gtest.h>
 #include <cstdint>
+#include <random>
 
+const size_t KEY_COUNT = 10;
+
+template <typename T>
 class XFastTrieTest : public ::testing::Test {
 protected:
-	XFastTrie<uint64_t> trie64_;
-	XFastTrie<uint32_t> trie32_;
-	XFastTrie<uint16_t> trie16_;
-	XFastTrie<uint8_t> trie8_;
-	std::vector<uint64_t> vec64_{0x9162c2ef9502b62c,
-								0x0ed183c7f286b882,
-								0xc9a26ad137b1ec35,
-								0x1fe16931d5c31bb6,
-								0x7788bf0bc23c344a,
-								0x80f637e26ec3bece,
-								0xd06dfed0fe7ea4cc,
-								0x81b36fb73a43b39d,
-								0xe5abbf5a7cf263f6,
-								0xcb77063e430dff9a};
-	std::vector<uint32_t> vec32_{0x16c78e43,
-								0xb9b2b735,
-								0x2dbc0f7f,
-								0xe38e14d8,
-								0x2be4c684,
-								0x9e298897,
-								0x18c53535,
-								0xce1cac5a,
-								0x4aa54679,
-								0x12073a29};
-	std::vector<uint16_t> vec16_{0x653e,
-								0xdb30,
-								0x5b43,
-								0x81d1,
-								0xc214,
-								0xe9df,
-								0xf4c0,
-								0x9536,
-								0x5f90,
-								0x34ff};
-	std::vector<uint8_t> vec8_{	0x8c,
-								0xe5,
-								0x46,
-								0x35,
-								0x5f,
-								0xc5,
-								0xa0,
-								0x1a,
-								0x44,
-								0xc4};
+	static void SetUpTestSuite() {
+		if (keys_ptr == nullptr) {
+			keys_ptr = new std::vector<T>();
+			std::mt19937_64 mte;
+			std::uniform_int_distribution<T> dist(std::numeric_limits<T>::min(), 
+												std::numeric_limits<T>::max());
+			while (keys_ptr->size() != KEY_COUNT && keys_ptr->size() <= std::numeric_limits<T>::max()) {
+				keys_ptr->push_back(dist(mte));
+				std::sort(std::begin(*keys_ptr), std::end(*keys_ptr));
+				auto new_end = std::unique(std::begin(*keys_ptr), std::end(*keys_ptr));
+				keys_ptr->erase(new_end, std::end(*keys_ptr));
+			}
+			std::random_shuffle(std::begin(*keys_ptr), std::end(*keys_ptr));
+		}
+		assert(keys_ptr->size() > 2);
+	}
+
+	static void TearDownTestSuite() {
+		delete keys_ptr;
+		keys_ptr = nullptr;
+	}
+
+	XFastTrie<T> trie;
+	static std::vector<T>* keys_ptr;
 };
 
-TEST_F(XFastTrieTest, IsEmptyOnCreation) {
-	EXPECT_EQ(trie64_.size(), 0);
-	EXPECT_EQ(trie32_.size(), 0);
-	EXPECT_EQ(trie16_.size(), 0);
-	EXPECT_EQ(trie8_.size(), 0);
+template <class T>
+std::vector<T>* XFastTrieTest<T>::keys_ptr = nullptr;
+
+typedef testing::Types<uint8_t, uint16_t, uint32_t, uint64_t> Implementations;
+
+TYPED_TEST_SUITE(XFastTrieTest, Implementations);
+
+TYPED_TEST(XFastTrieTest, IsEmptyOnCreation) {
+	EXPECT_EQ(this->trie.size(), 0);
 }
 
-TEST_F(XFastTrieTest, HasCorrectLimit) {
-	EXPECT_EQ(trie64_.limit(), 0xFFFFFFFFFFFFFFFF);
-	EXPECT_EQ(trie32_.limit(), 0xFFFFFFFF);
-	EXPECT_EQ(trie16_.limit(), 0xFFFF);
-	EXPECT_EQ(trie8_.limit(), 0xFF);
+TYPED_TEST(XFastTrieTest, HasCorrectLimit) {
+	EXPECT_EQ(this->trie.limit(), std::numeric_limits<TypeParam>::max());
 }
 
-TEST_F(XFastTrieTest, ContainsWorksOnEmpty) {
-	EXPECT_EQ(trie64_.contains(0), false);
-	EXPECT_EQ(trie32_.contains(0), false);
-	EXPECT_EQ(trie16_.contains(0), false);
-	EXPECT_EQ(trie8_.contains(0), false);
+TYPED_TEST(XFastTrieTest, ContainsWorksOnEmpty) {
+	for (const auto key : (*this->keys_ptr))
+		EXPECT_EQ(this->trie.contains(key), false);
 }
 
-TEST_F(XFastTrieTest, RemoveWorksOnEmpty) {
-	trie64_.remove(0);
-	trie32_.remove(0);
-	trie16_.remove(0);
-	trie8_.remove(0);
-}
-
-TEST_F(XFastTrieTest, MinWorksOnEmpty) {
-	EXPECT_EQ(trie64_.min().has_value(), false);
-	EXPECT_EQ(trie32_.min().has_value(), false);
-	EXPECT_EQ(trie16_.min().has_value(), false);
-	EXPECT_EQ(trie8_.min().has_value(), false);
-}
-
-TEST_F(XFastTrieTest, MaxWorksOnEmpty) {
-	EXPECT_EQ(trie64_.max().has_value(), false);
-	EXPECT_EQ(trie32_.max().has_value(), false);
-	EXPECT_EQ(trie16_.max().has_value(), false);
-	EXPECT_EQ(trie8_.max().has_value(), false);
-}
-
-TEST_F(XFastTrieTest, PredecessorWorksOnEmpty) {
-	EXPECT_EQ(trie64_.predecessor(0xFFFFFFFFFFFFFFFF).has_value(), false);
-	EXPECT_EQ(trie32_.predecessor(0xFFFFFFFF).has_value(), false);
-	EXPECT_EQ(trie16_.predecessor(0xFFFF).has_value(), false);
-	EXPECT_EQ(trie8_.predecessor(0xFF).has_value(), false);
-}
-
-TEST_F(XFastTrieTest, SuccessorWorksOnEmpty) {
-	EXPECT_EQ(trie64_.successor(0).has_value(), false);
-	EXPECT_EQ(trie32_.successor(0).has_value(), false);
-	EXPECT_EQ(trie16_.successor(0).has_value(), false);
-	EXPECT_EQ(trie8_.successor(0).has_value(), false);
-}
-
-TEST_F(XFastTrieTest, InsertAndCheckValues) {
-	for (uint64_t val : vec64_) {
-		EXPECT_EQ(trie64_.contains(val), false);
-		trie64_.insert(val);
-		EXPECT_EQ(trie64_.contains(val), true);
-	}
-
-	for (uint32_t val : vec32_) {
-		EXPECT_EQ(trie32_.contains(val), false);
-		trie32_.insert(val);
-		EXPECT_EQ(trie32_.contains(val), true);
-	}
-
-	for (uint16_t val : vec16_) {
-		EXPECT_EQ(trie16_.contains(val), false);
-		trie16_.insert(val);
-		EXPECT_EQ(trie16_.contains(val), true);
-	}
-
-	for (uint8_t val : vec8_) {
-		EXPECT_EQ(trie8_.contains(val), false);
-		trie8_.insert(val);
-		EXPECT_EQ(trie8_.contains(val), true);
+TYPED_TEST(XFastTrieTest, RemoveWorksOnEmpty) {
+	for (const auto key : (*this->keys_ptr)) {
+		this->trie.remove(key);
+		EXPECT_EQ(this->trie.size(), 0);
 	}
 }
 
-TEST_F(XFastTrieTest, DoubleInsertIsNoop) {
-	for (uint64_t val : vec64_) {
-		EXPECT_EQ(trie64_.contains(val), false);
-		trie64_.insert(val);
-		auto size = trie64_.size();
-		EXPECT_EQ(trie64_.contains(val), true);
-		trie64_.insert(val);
-		EXPECT_EQ(trie64_.size(), size);
-		EXPECT_EQ(trie64_.contains(val), true);
-	}
+TYPED_TEST(XFastTrieTest, MinWorksOnEmpty) {
+	EXPECT_EQ(this->trie.min().has_value(), false);
+}
 
-	for (uint32_t val : vec32_) {
-		EXPECT_EQ(trie32_.contains(val), false);
-		trie32_.insert(val);
-		size_t size = trie32_.size();
-		EXPECT_EQ(trie32_.contains(val), true);
-		trie32_.insert(val);
-		EXPECT_EQ(trie32_.size(), size);
-		EXPECT_EQ(trie32_.contains(val), true);
-	}
+TYPED_TEST(XFastTrieTest, MaxWorksOnEmpty) {
+	EXPECT_EQ(this->trie.max().has_value(), false);
+}
 
-	for (uint16_t val : vec16_) {
-		EXPECT_EQ(trie16_.contains(val), false);
-		trie16_.insert(val);
-		size_t size = trie16_.size();
-		EXPECT_EQ(trie16_.contains(val), true);
-		trie16_.insert(val);
-		EXPECT_EQ(trie16_.size(), size);
-		EXPECT_EQ(trie16_.contains(val), true);
-	}
+TYPED_TEST(XFastTrieTest, PredecessorWorksOnEmpty) {
+	for (const auto key : (*this->keys_ptr))
+		EXPECT_EQ(this->trie.predecessor(key).has_value(), false);
+}
 
-	for (uint8_t val : vec8_) {
-		EXPECT_EQ(trie8_.contains(val), false);
-		trie8_.insert(val);
-		size_t size = trie8_.size();
-		EXPECT_EQ(trie8_.contains(val), true);
-		trie8_.insert(val);
-		EXPECT_EQ(trie8_.size(), size);
-		EXPECT_EQ(trie8_.contains(val), true);
+TYPED_TEST(XFastTrieTest, SuccessorWorksOnEmpty) {
+	for (const auto key : (*this->keys_ptr))
+		EXPECT_EQ(this->trie.successor(key).has_value(), false);
+}
+
+TYPED_TEST(XFastTrieTest, InsertAndCheckValues) {
+	for (const auto key : (*this->keys_ptr)) {
+		this->trie.insert(key);
+		EXPECT_EQ(this->trie.contains(key), true);
 	}
 }
 
-TEST_F(XFastTrieTest, InsertAndRemove) {
-	for (uint64_t val : vec64_) {
-		EXPECT_EQ(trie64_.contains(val), false);
-		trie64_.insert(val);
-		EXPECT_EQ(trie64_.contains(val), true);
+TYPED_TEST(XFastTrieTest, DoubleInsertIsNoop) {
+	for (const auto key : (*this->keys_ptr)) {
+		this->trie.insert(key);
+		auto size = this->trie.size();
+		EXPECT_EQ(this->trie.contains(key), true);
+		this->trie.insert(key);
+		EXPECT_EQ(this->trie.size(), size);
+		EXPECT_EQ(this->trie.contains(key), true);
 	}
-	EXPECT_EQ(trie64_.size(), vec64_.size());
-
-	for (uint64_t val : vec64_) {
-		EXPECT_EQ(trie64_.contains(val), true);
-		trie64_.remove(val);
-		EXPECT_EQ(trie64_.contains(val), false);
-	}
-	EXPECT_EQ(trie64_.size(), 0);
-
-	for (uint32_t val : vec32_) {
-		EXPECT_EQ(trie32_.contains(val), false);
-		trie32_.insert(val);
-		EXPECT_EQ(trie32_.contains(val), true);
-	}
-	EXPECT_EQ(trie32_.size(), vec32_.size());
-
-	for (uint32_t val : vec32_) {
-		EXPECT_EQ(trie32_.contains(val), true);
-		trie32_.remove(val);
-		EXPECT_EQ(trie32_.contains(val), false);
-	}
-	EXPECT_EQ(trie32_.size(), 0);
-
-	for (uint16_t val : vec16_) {
-		EXPECT_EQ(trie16_.contains(val), false);
-		trie16_.insert(val);
-		EXPECT_EQ(trie16_.contains(val), true);
-	}
-	EXPECT_EQ(trie16_.size(), vec16_.size());
-
-	for (uint16_t val : vec16_) {
-		EXPECT_EQ(trie16_.contains(val), true);
-		trie16_.remove(val);
-		EXPECT_EQ(trie16_.contains(val), false);
-	}
-	EXPECT_EQ(trie16_.size(), 0);
-
-	for (uint8_t val : vec8_) {
-		EXPECT_EQ(trie8_.contains(val), false);
-		trie8_.insert(val);
-		EXPECT_EQ(trie8_.contains(val), true);
-	}
-	EXPECT_EQ(trie8_.size(), vec8_.size());
-
-	for (uint8_t val : vec8_) {
-		EXPECT_EQ(trie8_.contains(val), true);
-		trie8_.remove(val);
-		EXPECT_EQ(trie8_.contains(val), false);
-	}
-	EXPECT_EQ(trie8_.size(), 0);
 }
 
-TEST_F(XFastTrieTest, TestPredecessorAndSuccessor) {
-	for (uint64_t val : vec64_) {
-		trie64_.insert(val);
-	}
-
-	std::sort(std::begin(vec64_), std::end(vec64_));
-
-	EXPECT_EQ(trie64_.predecessor(vec64_[0]).has_value(), false);
-	ASSERT_EQ(trie64_.successor(vec64_[0]).has_value(), true);
-	EXPECT_EQ(trie64_.successor(vec64_[0]).value(), vec64_[1]);
-
-	for (size_t i = 1; i < vec64_.size() - 1; ++i) {
-		ASSERT_EQ(trie64_.predecessor(vec64_[i]).has_value(), true);
-		ASSERT_EQ(trie64_.successor(vec64_[i]).has_value(), true);
-		EXPECT_EQ(trie64_.predecessor(vec64_[i]).value(), vec64_[i - 1]);
-		EXPECT_EQ(trie64_.successor(vec64_[i]).value(), vec64_[i + 1]);
-	}
-
-	ASSERT_EQ(trie64_.predecessor(vec64_[vec64_.size() - 1]).has_value(), true);
-	EXPECT_EQ(trie64_.predecessor(vec64_[vec64_.size() - 1]).value(), vec64_[vec64_.size() - 2]);
-	EXPECT_EQ(trie64_.successor(vec64_[vec64_.size() - 1]).has_value(), false);
-
-	for (uint32_t val : vec32_) {
-		trie32_.insert(val);
-	}
-
-	std::sort(std::begin(vec32_), std::end(vec32_));
-
-	EXPECT_EQ(trie32_.predecessor(vec32_[0]).has_value(), false);
-	ASSERT_EQ(trie32_.successor(vec32_[0]).has_value(), true);
-	EXPECT_EQ(trie32_.successor(vec32_[0]).value(), vec32_[1]);
-
-	for (size_t i = 1; i < vec32_.size() - 1; ++i) {
-		ASSERT_EQ(trie32_.predecessor(vec32_[i]).has_value(), true);
-		ASSERT_EQ(trie32_.successor(vec32_[i]).has_value(), true);
-		EXPECT_EQ(trie32_.predecessor(vec32_[i]).value(), vec32_[i - 1]);
-		EXPECT_EQ(trie32_.successor(vec32_[i]).value(), vec32_[i + 1]);
-	}
+TYPED_TEST(XFastTrieTest, InsertAndRemove) {
+	for (const auto key : (*this->keys_ptr))
+		this->trie.insert(key);
 	
-	ASSERT_EQ(trie32_.predecessor(vec32_[vec32_.size() - 1]).has_value(), true);
-	EXPECT_EQ(trie32_.predecessor(vec32_[vec32_.size() - 1]).value(), vec32_[vec32_.size() - 2]);
-	EXPECT_EQ(trie32_.successor(vec32_[vec32_.size() - 1]).has_value(), false);
-
-	for (uint16_t val : vec16_) {
-		trie16_.insert(val);
+	for (const auto key : (*this->keys_ptr)) {
+		this->trie.remove(key);
+		EXPECT_EQ(this->trie.contains(key), false);
 	}
+}
 
-	std::sort(std::begin(vec16_), std::end(vec16_));
-
-	EXPECT_EQ(trie16_.predecessor(vec16_[0]).has_value(), false);
-	ASSERT_EQ(trie16_.successor(vec16_[0]).has_value(), true);
-	EXPECT_EQ(trie16_.successor(vec16_[0]).value(), vec16_[1]);
-
-	for (size_t i = 1; i < vec16_.size() - 1; ++i) {
-		ASSERT_EQ(trie16_.predecessor(vec16_[i]).has_value(), true);
-		ASSERT_EQ(trie16_.successor(vec16_[i]).has_value(), true);
-		EXPECT_EQ(trie16_.predecessor(vec16_[i]).value(), vec16_[i - 1]);
-		EXPECT_EQ(trie16_.successor(vec16_[i]).value(), vec16_[i + 1]);
-	}
+TYPED_TEST(XFastTrieTest, TestPredecessorAndSuccessor) {
+	for (const auto key : (*this->keys_ptr))
+		this->trie.insert(key);
 	
-	ASSERT_EQ(trie16_.predecessor(vec16_[vec16_.size() - 1]).has_value(), true);
-	EXPECT_EQ(trie16_.predecessor(vec16_[vec16_.size() - 1]).value(), vec16_[vec16_.size() - 2]);
-	EXPECT_EQ(trie16_.successor(vec16_[vec16_.size() - 1]).has_value(), false);
+	std::sort(std::begin((*this->keys_ptr)), std::end((*this->keys_ptr)));
 
-	for (uint8_t val : vec8_) {
-		trie8_.insert(val);
+	auto predecessor = this->trie.predecessor((*this->keys_ptr)[0]);
+	auto successor = this->trie.successor((*this->keys_ptr)[0]);
+
+	EXPECT_EQ(predecessor.has_value(), false);
+	ASSERT_EQ(successor.has_value(), true);
+	EXPECT_EQ(successor.value(), (*this->keys_ptr)[1]);
+
+	for (size_t i = 1; i < (*this->keys_ptr).size() - 1; ++i) {
+		predecessor = this->trie.predecessor((*this->keys_ptr)[i]);
+		successor = this->trie.successor((*this->keys_ptr)[i]);
+
+		ASSERT_EQ(predecessor.has_value(), true);
+		EXPECT_EQ(predecessor.value(), (*this->keys_ptr)[i - 1]);
+		ASSERT_EQ(successor.has_value(), true);
+		EXPECT_EQ(successor.value(), (*this->keys_ptr)[i + 1]);
 	}
 
-	std::sort(std::begin(vec8_), std::end(vec8_));
+	predecessor = this->trie.predecessor((*this->keys_ptr)[(*this->keys_ptr).size() - 1]);
+	successor = this->trie.successor((*this->keys_ptr)[(*this->keys_ptr).size() - 1]);
 
-	EXPECT_EQ(trie8_.predecessor(vec8_[0]).has_value(), false);
-	ASSERT_EQ(trie8_.successor(vec8_[0]).has_value(), true);
-	EXPECT_EQ(trie8_.successor(vec8_[0]).value(), vec8_[1]);
-
-	for (size_t i = 1; i < vec8_.size() - 1; ++i) {
-		ASSERT_EQ(trie8_.predecessor(vec8_[i]).has_value(), true);
-		ASSERT_EQ(trie8_.successor(vec8_[i]).has_value(), true);
-		EXPECT_EQ(trie8_.predecessor(vec8_[i]).value(), vec8_[i - 1]);
-		EXPECT_EQ(trie8_.successor(vec8_[i]).value(), vec8_[i + 1]);
-	}
-	
-	ASSERT_EQ(trie8_.predecessor(vec8_[vec8_.size() - 1]).has_value(), true);
-	EXPECT_EQ(trie8_.predecessor(vec8_[vec8_.size() - 1]).value(), vec8_[vec8_.size() - 2]);
-	EXPECT_EQ(trie8_.successor(vec8_[vec8_.size() - 1]).has_value(), false);
+	ASSERT_EQ(predecessor.has_value(), true);
+	EXPECT_EQ(predecessor.value(), (*this->keys_ptr)[(*this->keys_ptr).size() - 2]);
+	EXPECT_EQ(successor.has_value(), false);
 }
