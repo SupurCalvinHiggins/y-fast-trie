8646f2a88d522cda28dce4f71935f25b65fe1755
ethan-j-carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2022-03-15 21:10:18 -0400

    Renamed RBTree.h and Node.h, added deletion
    I gave the header files their proper names and got deletion working.

--- red-black-tree/main.cpp
+++ red-black-tree/main.cpp
@@ -1,4 +1,4 @@
-#include "RBTree.h"
+#include "red-black-tree.h"
 
 const int SEED = 0;
 const int TESTS = 10000;
@@ -18,25 +18,35 @@ std::vector<int> get_keys() {
 int main(void) {
 	srand(SEED);
 	std::vector<int> keys = get_keys();
-	RBTree<int> tree = RBTree<int>();
-	for (int key : keys) {
-		std::cout << "Inserting " << std::to_string(key) << "\n\n";
-		tree.insert(key);
-		//tree.show(tree.root(), 0);
+	RedBlackTree<int> tree = RedBlackTree<int>();
+	unsigned int indices = keys.size();
+	for (unsigned int i = 0; i < indices; i++) {
+		std::cout << "\nInserting " << std::to_string(keys[i]) << "\n\n";
+		tree.insert(keys[i]);
+		tree.show(tree.root(), 0);
 		std::cout << '\n';
+		if (!tree.check_balance()) {
+			std::cout << "Tree is unbalanced.Stopping tests" << '\n';
+			break;
+		}
 	}
 
-	int indices = keys.size();
-
-	while (indices > 0) {
-		int key = keys[(rand() % (indices))];
-		std::cout << "Removing " << std::to_string(key) << "\n\n";
+	while (indices > 0 && tree.check_balance()) {
+		int index = rand() % (indices);
+		int key = keys[index];
+		if (key == 240) {
+			key = 240;
+		}
+		keys.erase(keys.begin() + index);
+		std::cout << "\nRemoving " << std::to_string(key) << "\n\n";
 		tree.remove(key);
-		//tree.show(tree.root(), 0);
-		
+		if (tree.root() != nullptr) {
+			tree.show(tree.root(), 0);
+		}
 		indices--;
-	}
 
+		
+	}
 	std::cout << '\n';
 	std::cout << std::to_string(tree.root()->key()) << '\n';
 }
