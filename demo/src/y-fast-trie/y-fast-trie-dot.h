2699b2351dda69bacad5f71e3099fd4adfdb4d1e
SupurCalvinHiggins <calvin@supurloop.com>
2022-04-18 19:00:56 -0400

    Added basic code for dot file generation. Started working on #43.

---
+++ demo/src/y-fast-trie/y-fast-trie-dot.h
@@ -0,0 +1,46 @@
+#include "../../../src/y-fast-trie/y-fast-trie.h"
+#include <string>
+#include <sstream>
+
+template <typename Key_>
+class YFastTrieDot : public YFastTrie<Key_> {
+private:
+    std::string ptr_to_str(void* ptr) const noexcept {
+        std::ostringstream ss;
+        ss << ptr;
+        return "n" + ss.str();
+    }
+
+    std::string key_to_str(key_type key) const noexcept {
+        std::ostringstream ss;
+        ss << key;
+        return ss.str();
+    }
+
+public:
+    std::string to_dot() const noexcept {
+        std::string output;
+        output += "digraph {\n"
+
+        for (const auto& level : this->lss_) {
+            std::string level_output;
+            level_output += "{rank = same; ";
+
+            for (const auto& key_node_pair : level) {
+                auto key = key_node_pair.first;
+                auto node = key_node_pair.second;
+                if (node->get_left()) 
+                    output += "\t" + ptr_to_str(node) + " -> " + ptr_to_str(node->get_left()) + "\n";
+                if (node->get_right()) 
+                    output += "\t" + ptr_to_str(node) + " -> " + ptr_to_str(node->get_right()) + "\n";    
+                output += "\t" + ptr_to_str(node) + "[label=\"" + key_to_str(key) + "\"]\n";
+                level_output += ptr_to_str(node) + "; ";
+            }
+
+            level_output += "}";
+            output += "\t" + level_output + "\n"; 
+        }
+        output += "}";
+        return output;
+    }
+};
