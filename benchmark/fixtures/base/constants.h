6c632b93ca22b09d2ee3e4494c569ece1e283f2f
ethan-j-carlson <xxswagnem8xx2@gmail.com>
2022-04-24 18:30:15 -0400

    Did some clean up, added get

--- benchmark/fixtures/base/constants.h
+++ benchmark/fixtures/base/constants.h
@@ -2,4 +2,4 @@
 
 constexpr uint64_t RANGE_MULTIPLER   = (1 << 4);
 constexpr uint64_t RANGE_LOWER_BOUND = (1 << 4);
-constexpr uint64_t RANGE_UPPER_BOUND = (1 << 16);
+constexpr uint64_t RANGE_UPPER_BOUND = (1 << 20);
