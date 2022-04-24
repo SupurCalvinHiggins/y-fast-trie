a0196c4c66e9cf9bbe682c756235b0e072dd24fe
SupurCalvinHiggins <calvin@supurloop.com>
2022-04-23 22:10:17 -0400

    Made demo faster and added terrible animation toggle.

--- demo/src/red-black-tree/red-black-tree.h
+++ demo/src/red-black-tree/red-black-tree.h
@@ -42,6 +42,11 @@ public:
 
 //Contructor and destructor
 public:
+    bool animate_;
+
+    void set_animate(bool animate) {
+        animate_ = animate;
+    }
 
     /**
      * @brief ruct a new RedBlackTree object
@@ -52,6 +57,7 @@ public:
         max_ = nullptr;
         min_ = nullptr;
         size_ = 0;
+        animate_ = true;
         std::vector<node_ptr> node_path_;
     };
 
@@ -275,12 +281,17 @@ private:
 public:
     void MARK(node_ptr node){
         node_path_.push_back(node);
+        if (animate_) 
+            UPDATE_GUI();
+    }
+
+    void CLEAN() noexcept{
+        node_path_.clear();
         UPDATE_GUI();
     }
 
-    void CLEAN()  noexcept{
+    void CLEAN_NO_UPDATE() noexcept{
         node_path_.clear();
-        UPDATE_GUI();
     }
 
     /**
